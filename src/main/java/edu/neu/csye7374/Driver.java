249f368a1b1c65bcb24922f86a2e3b3e2dcc55f5
Sai Shreya Kondagari <kondagari.s@northeastern.edu>
2025-05-20 21:22:14 -0400

    updated driver

--- src/main/java/edu/neu/csye7374/Driver.java
+++ src/main/java/edu/neu/csye7374/Driver.java
@@ -10,8 +10,8 @@ public class Driver {
 	public static void main(String[] args) {
 		System.out.println("============Main Execution Start===================\n\n");
 
-         //Add your code in between these two print statements
-		
+        //Add your code in between these two print statements
+		StockMarket.demo();
 		 
 		System.out.println("\n\n============Main Execution End===================");
 	}
